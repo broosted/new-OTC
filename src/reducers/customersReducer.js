0532d661bc748b85e926ee8a04492095d3696859
broosted <matrix.10.tad@gmail.com>
2022-08-07 00:10:56 -0700

    added reducers

--- src/reducers/customersReducer.js
+++ src/reducers/customersReducer.js
@@ -1,4 +1,5 @@
 const initialState = {
+    status: 'idle',
     customers: [
         {
             "customer_name": "Jenny",
@@ -13,7 +14,50 @@ const initialState = {
 export default function customersReducer(state = initialState, action) {
     // The reducer normally looks at the action type field to decide what happens
     switch (action.type) {
-        // Do something here based on the different types of actions
+        case 'customers/customerFetchAll':
+            return {
+                ...state,
+                customers: [...state.customers, action.payload]
+            }
+        case 'customers/customerUpdate':
+            return {
+                ...state,
+                customers: state.customers.map(customer => {
+                    if(customer['_id'] === action.id) {
+                        return {...customer,
+                            "customer_name": action.payload['customer_name'],
+                            "customer_rating": action.payload['customer_rating'],
+                            "customer_review": action.payload['customer_review'],
+                            "customer_id": action.payload['customer_id']
+                        }
+                    }
+                    return customer
+                }),
+                status: 'UPDATED'
+            }
+        case 'customers/customerCreate':
+            return {
+                ...state,
+                customers: [...state.customers, action.payload],
+                status: 'CREATED'
+            }
+        case 'customers/customerDelete':
+            return {
+                ...state,
+                customers: state.customers.filter(customer => (customer['_id'] === action.id)),
+                status: 'DELETED'
+            }
+        case 'customers/customerFetchById':
+            return {
+                ...state,
+                customers: state.customers.map(customer => {
+                    if(customer['_id'] === action.id) {
+                        return action.payload
+                    }
+                    return customer
+                }),
+                status: 'UPDATED'
+            }
         default:
             // If this reducer doesn't recognize the action type, or doesn't
             // care about this specific action, return the existing state unchanged
